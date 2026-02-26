9b29da6bcc4088835371859c40152cb3930be9d0
AndrewOnlysx <onlysexofficialpage@gmail.com>
2026-02-25 20:03:31 -0400

    fix: update parameter type to promise in Page component

--- app/(dashboard)/pageClients/models/[id]/page.tsx
+++ app/(dashboard)/pageClients/models/[id]/page.tsx
@@ -1,15 +1,13 @@
 import { GetModelById } from '@/database/actions/models/GetModelById'
 import { NextPage } from 'next'
 import EditModelForm from './components/EditModelForm'
-
+import { use } from "react";
 interface Props {
-    params: {
-        id: string
-    }
+    params: Promise<{ id: string }>
 }
 
 const Page = async ({ params }: Props) => {
-    const { id } = params
+    const { id } = await params
     const { model, ok } = await GetModelById(id)
     return <div><EditModelForm model={model} /></div>
 }
